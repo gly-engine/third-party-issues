4d66f60ab9041e4b67cdcbacbf3388136e47a6a0
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-10 15:26:19 -0300

    feat: support multiple ID in media player

--- ginga-media-player/demo/app.lua
+++ ginga-media-player/demo/app.lua
@@ -2,18 +2,26 @@ local canvas = canvas
 local event = event 
 local delta = 100
 local ccws_finished, ccws_status, ccws_error, ccws_body, ccws_post = '', '', '', '', ''
-local base_url = 'http://localhost:44642/dtv/mediaplayers/1'
+local base_url = 'http://localhost:44642/dtv/mediaplayers/{id}'
+local base_url_all = 'http://localhost:44642/dtv/mediaplayers'
 local base_url_cap = 'http://localhost:44642/dtv/platform-capabilities'
 local video_url = 'https://yt-dash-mse-test.commondatastorage.googleapis.com/media/car-20120827-manifest.mpd'
 local action_play = '{"url":"%s","action":"%s","pos":{"x":%d,"y":%d,"w":%d,"h":%d}}'
 local action_stop = '{"action":"unload"}'
 local headers = {['User-Agent'] = 'Ginga (GlyOS;SmartTv/Linux)'}
-local actions = {'prepare','start','pause','resume','stop','unload', 'get-mp', 'get-pc'}
+local actions = {'prepare','start','pause','resume','stop','unload', 'get-mp{id}', 'get-mps', 'get-pc', 'change-id'}
 local menu = 1
 local debounce = 300
 local lasttime = -9999
 local lastevt = 'evt'
 local now = 0
+local id = 1
+
+local urls = {
+    [7] = base_url,
+    [8] = base_url_all,
+    [9] = base_url_cap
+}
 
 local function dump(tbl)
     local result = {}
@@ -23,14 +31,17 @@ local function dump(tbl)
     return table.concat(result, ", ")
 end
 
-local function do_something()
+local function do_something(confirm)
     ccws_finished, ccws_status, ccws_error, ccws_body = '', '', '', ''
-    if menu == 7 or menu == 8 then
+    if menu == 10 then
+        if confirm and id <= 5 then id = id + 1 end
+        if not confirm and id > 1 then id = id - 1 end
+    elseif 7 <= menu or menu <= 9 then
         local r1, r2 = event.post({
             class = 'http',
             type = 'request',
             method = 'get',
-            uri = (menu == 7 and base_url) or base_url_cap,
+            uri = urls[menu]:gsub('{id}', id),
             headers = headers
         })
         ccws_post = tostring(r1)..' '..tostring(r2)
@@ -39,7 +50,7 @@ local function do_something()
             class = 'http',
             type = 'request',
             method = 'post',
-            uri = base_url,
+            uri = base_url:gsub('{id}', id),
             headers = headers,
             body = action_stop
         })
@@ -50,7 +61,7 @@ local function do_something()
             class = 'http',
             type = 'request',
             method = 'post',
-            uri = base_url,
+            uri = base_url:gsub('{id}', id),
             headers = headers,
             body = body
         })
@@ -63,16 +74,15 @@ local function handler(evt)
     if evt.class == 'key' and evt.type == 'press' then
         if menu > 1 and evt.key == 'CURSOR_UP' then
             menu = menu - 1
-        elseif menu < 8 and evt.key == 'CURSOR_DOWN' then
+        elseif menu < #actions and evt.key == 'CURSOR_DOWN' then
             menu = menu + 1
-        elseif evt.key == 'CURSOR_RIGHT' and (now - lasttime) > debounce then
-            print('clicado')
+        elseif (evt.key == 'CURSOR_LEFT' or evt.key == 'CURSOR_RIGHT') and (now - lasttime) > debounce then
             lasttime = now
-            do_something()
+            do_something(evt.key == 'CURSOR_RIGHT')
         end
     end
     if evt.class == 'http' then
-        if evt.body ~= nil then ccws_body = ccws_body..tostring(evt.body):gsub('\r', ''):gsub('\n', '') end
+        if evt.body ~= nil then ccws_body = ccws_body..tostring(evt.body):gsub('\r', ''):gsub('\n', ''):gsub('\t', '') end
         if evt.finished ~= nil then ccws_finished = tostring(evt.finished) end
         if evt.error ~= nil then ccws_error = tostring(evt.error) end
         if evt.code ~= nil then ccws_status = tostring(evt.code) end
@@ -88,7 +98,7 @@ local function tick()
     canvas:drawText(6, 8 + ((menu-1) * 24), '>')
     canvas:attrColor('white')
     for i = 1, #actions do
-        canvas:drawText(20, 8 + ((i-1) * 24), actions[i])
+        canvas:drawText(20, 8 + ((i-1) * 24), actions[i]:gsub('{id}', id))
     end
     canvas:drawText(6, 312, 'ccws finished: '..ccws_finished)
     canvas:drawText(6, 344, 'ccws status: '..ccws_status)
